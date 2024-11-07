6d70c9d4b8e15de5f1494ee8983b826134dc0cee
esckles <davidsamzzywilliams1234@gmail.com>
2024-11-07 16:16:29 +0100

    hero updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Hero from "./Components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default App;

a7c2a07214f896e43709283b34440be4f8605a05
esckles <davidsamzzywilliams1234@gmail.com>
2024-11-07 16:27:34 +0100

    hero pushed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HeaderComp from "./Components/HeaderComp";
+import Hero from "./Components/Hero";
 
 const App = () => {
   return (
     <div>
       <HeaderComp />
+      <Hero />
     </div>
   );
 };

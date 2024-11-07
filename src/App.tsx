e80c35e5c5f6b8270991d469de1daf85c08997ed
esckles <davidsamzzywilliams1234@gmail.com>
2024-11-07 17:18:37 +0100

    yellow bg

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeaderComp from "./Components/HeaderComp";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
+import Bg from "./Components/bg";
 
 const App = () => {
   return (
     <div>
       <HeaderComp />
       <Hero />
+      <Bg />
       <Footer />
     </div>
   );
---
+++ src/Components/bg.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bg = () => {
+  return <div className="w-full h-[50vh] bg-yellow-400">Bg</div>;
+};
+
+export default Bg;

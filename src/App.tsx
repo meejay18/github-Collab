0a4082e4f4f2174efc3fefdac464faf9f37adbbc
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-11-07 17:08:30 +0100

    footer update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderComp from "./Components/HeaderComp";
 import Hero from "./Components/Hero";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
     <div>
       <HeaderComp />
       <Hero />
+      <Footer />
     </div>
   );
 };
---
+++ src/Components/Footer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="w-full, h-[20rem] blur-md bg-[black]"></div>;
+};
+
+export default Footer;
